2461e73d22ffecf24e2cdd8dfc4b81a90d3ece77
Your Name <you@example.com>
2017-07-08 12:13:32 +0100

    Finish biometrics

--- content.js
+++ content.js
@@ -10,6 +10,18 @@
                 "Put it like this, a just sounds like a kind of database with built-in validation — which it is.",
                 "However, the clever bit is that the data is not stored in a one single location or managed by any particular person. Instead, it is said to be distributed, existing on several computers at the same time in such a way that anybody with an interest can maintain a copy. "
             ]
+        },
+        "biometrics": {
+            "title": "BIOMETRICS",
+            "teaserText": "Security using your physical traits.",
+            "youtubeLink": "https://www.youtube.com/embed/88Rjg8gM_DI",
+            "paragraphs": [
+                "Biometrics is using a person’s physical traits ( fingerprints , iris , veins etc) to identify them.This has many uses , such as allowing someone to access their accounts without them having to remember their passwords, while still being as secure ( or even more) as a password.",
+                "<h3>Types of biometrics:</h3><br> Fingerprint recognition<br> Iris recognition<br> Facial recognition<br> Voice recognition",
+                "Which biometrics to use depends on how secure you need to be. You may even want a combination of them.",
+                "<h3>Biometrics in practice</h3>",
+                "Biometrics isn’t just useful for businesses , it’s useful for even the common person, If you have ever reused a password or got frustrated at how many you had to remember, Biometrics can help you!"
+            ]
         }
     }
 
