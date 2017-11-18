ada36326a4bbb38810e27dec0f4b1acdc38f1fd8
Blockboy1294 <bazzakane@gmail.com>
2017-11-18 12:04:57 +0000

    Quick edit to blockchain

--- Website/content.js
+++ Website/content.js
@@ -32,17 +32,17 @@
         },
         "blockchain":{
             "title":"Blockchain",
-            "teaserText": "A new way to securely store information!",
+            "teaserText": "A new way to securely transfer assets!",
             "youtubeLink": "https://www.youtube.com/embed/IhP3J0j9JmY",
             "paragraphs": [
-                	"Blockchain is a new way of storing information. It uses multiple servers to store the same information so that if one is attacked the data is not lost.",
+                	"Blockchain is a new way of transferring assets! It uses multiple servers to store the same information so that if one is attacked the data is not lost.",
 			"Put it like this, a just sounds like a kind of database with built-in validation—which it is. ",
 			"However, the clever bit is that the data is not stored in a one individual location or managed by any single person. Instead, it is said to be distributed, existing on several computers at the same time in such a way that anybody with an interest can maintain a copy. ",
 			"Better still, the block validation system ensures that nobody can change the records. Rather, old transactions are preserved forever and new transactions are added to the data irreversibly.",
 			"Effectively a blockchain is a kind of independent, transparent, and permanent database coexisting in multiple locations and shared by a community. Therefore, it’s sometimes referred to as a mutual distributed ledger (MDL). ",
 			"Blockchains are incredibly secure against attacks, as a potential attacker would have to attack all the servers at once, rather than attacking one and everything going down. There could be thousands of different machines, and all would have to be attacked!",
 			"This would require tons upon tons of power to attack, and that is simply too impractical.", 
-			"There’s nothing new about MDLs, their origins traceable to 1976. But for a long time, they were regarded as complicated and not altogether safe.",
+			"There’s nothing new about Mutual Distributed Ledgers, their origins traceable to 1976. But for a long time, they were regarded as complicated and not altogether safe.",
 			"But if the registry was not owned by a central third party but sitting on multiple machines and everybody had copies, it would have resilience and looking up transactions would be quick. With the data being immutable once entered in the ledger, it would provide a permanent record that financial regulators and auditors could quickly fall in love with.",
 			"In other words, it’s a souped-up audit trail for anything you like, not just a cryptocurrency. It’s not just one system. Indeed, the situation can be compared of the database revolution of the 1970s: there wasn’t just one type or structure for a database, you created the specific database you wanted for your own purposes."	
             ]
