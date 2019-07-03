7b51559e2e20b772880d239ff74c4d044591c0da
Wizzdom <dm.connor2003@gmail.com>
2019-07-03 19:22:53 +0100

    Revert "Merge branch 'master' of https://github.com/BePaySmart/BePaySmart.org"
    This reverts commit 7947d3c65c3cab3685590229d58fa2bb45b33dbf, reversing
    changes made to ab4bcb13c7efb7640f815c3d09c08bdf5bb9a4cb.

--- Website/content.js
+++ Website/content.js
@@ -57,7 +57,7 @@
 	    "paragraphs": [
 		        "Contactless payments are a quicker and easier way to pay. Instead of carrying a bunch of coins in your pocket just use contactless payments.",
 		        "There are loads of ways to do contactless payments; for example, tap-to-pay credit cards, tap-to-pay debit cards, certain key fobs, smart cards or other devices including smartphones and some other mobile devices.",
-		        "Because no signature or PIN verification is typically required, contactless purchases are typically limited to a set amount per transaction, known as a \"floor limit\". Most banks set this to 25 to 30 euros, although that may differ. Transactions above this usually require a type of identification, such as a PIN or <a href=\"index.html?page=biometrics\">fingerprint</a>.",
+		        "Because no signature or PIN verification is typically required, contactless purchases are typically limited to a set amount per transaction, known as a \"floor limit\". Most banks set this to 25 to 30 euros, although that may differ. Transactions above this usually require a type of identification, such as a PIN or <a href=\"page.html?page=biometrics\">fingerprint</a>.",
 		        "Mobil were one of the first companies to use contactless payments with their speedpass cards in 1997. Mobil used the speed pass at their gas stations for a quick and easy way to pay. The speedpass has a dedicated transponder identification and a security code making it more secure than a debit card.",
 
             ]
