e2168c367ece42f236511b140e3be9af47f874d6
Barry Kane <bazzakane@gmail.com>
2019-07-03 18:45:40 +0100

    fixed my mistaken identity of repos

--- Website/app.js
+++ Website/app.js
@@ -5,18 +5,18 @@ app = express()
 
 PORT = process.env.PORT || 80
 
-app.get("/pageJSON/:page", function(req, res) {
+app.get("/pageJSON/:page",function(req,res){
     page = req.params.page;
     res.json(content.page(page))
 })
 
-app.get("/pagesJSON", function(req, res) {
+app.get("/pagesJSON",function (req,res){
     res.json(content.pages())
 })
 
 app.use(express.static("static"))
 
 
-app.listen(PORT, function() {
-    console.log("app running on port " + PORT)
+app.listen(PORT,function(){
+    console.log("Server is now running on port: " + PORT)
 })
