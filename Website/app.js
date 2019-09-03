276518c7a24f16a48429927e0013e607f1ddc4b6
Barry Kane <bazzakane@gmail.com>
2019-09-03 21:25:49 +0100

    Please oh god let ssl work

--- Website/app.js
+++ Website/app.js
@@ -7,15 +7,15 @@ var content = require("./content");
 const app = express();
 
 // Certificate
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/bepaysmart.org/chain.pem', 'utf8');
 
-// const credentials = {
-// 	key: privateKey,
-// 	cert: certificate,
-// 	ca: ca
-// };
+const credentials = {
+	key: privateKey,
+ 	cert: certificate,
+ 	ca: ca
+};
 
 // Get content from JSON file
 app.get("/pageJSON/:page", function(req, res) {
@@ -31,12 +31,12 @@ app.use(express.static("static"));
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-// httpsServer.listen(443, () => {
-// 	console.log('HTTPS Server running on port 443');
-// });
+httpsServer.listen(443, () => {
+ 	console.log('HTTPS Server running on port 443');
+ });
